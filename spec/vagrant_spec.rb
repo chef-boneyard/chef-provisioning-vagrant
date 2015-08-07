d3bce931a36308aafcc5a7a9a88c2f2e594c96c9
Chris Doherty <cdoherty@chef.io>
2015-08-07 13:24:26 -0700

    Add an explicit dependency on cheffish >= 1.3.1 so we can use 'let' variables in rspec with impunity.

--- spec/vagrant_spec.rb
+++ spec/vagrant_spec.rb
@@ -1,11 +1,15 @@
 describe "Chef::Provisioning::Vagrant" do
   extend VagrantSupport
-  # include VagrantConfig   # uncomment to get `chef_config` or to mix in code.
 
   when_the_chef_12_server "exists", server_scope: :context, port: 8900..9000 do
     with_vagrant "integration tests" do
       context "machine resource" do
+        let(:let_var) { "a let variable in the enclosing context"}
+
         it "doesn't run any tests" do
+          expect_converge {
+            log "should be able to use let_var as '#{let_var}' with no error."
+          }.not_to raise_error
         end
       end
     end
